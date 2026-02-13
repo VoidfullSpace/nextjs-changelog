094a1997a91575ec5e7abbc3eae62a087fda385a
TaranaKhanna <taranakhanna5@gmail.com>
2026-02-13 06:05:32 +0000

    feat: add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { PostItem } from "./components/PostItem";
+import { Header } from "./components/Header";
 
 const updates = [
   {
@@ -51,8 +52,10 @@ const updates = [
 
 export default function Home() {
   return (
-    <main className="max-w-5xl mx-auto px-6 py-16 bg-gray-100">
-      {/* Header */}
+    <>
+    <Header />
+
+    <main className="mt-12 max-w-6xl mx-auto px-6 py-16 bg-gray-100 rounded-2xl">
       <h2 className="text-3xl font-semibold mb-2 text-gray-700">
         Product Updates
       </h2>
@@ -66,5 +69,6 @@ export default function Home() {
         ))}
       </div>
     </main>
+    </>
   );
 }
